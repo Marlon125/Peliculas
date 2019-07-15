03cab64b1b4e5544129ab2ba37365fc7af44672a
Marlon Anaya <desarrolladorsw@unicomfacauca.edu.co>
2019-07-15 16:45:34 -0500

    Add files to database administration

---
+++ app/src/main/java/com/marlon/peliculas/infrastructure/database/DbOpenHelper.kt
@@ -0,0 +1,2 @@
+package com.marlon.peliculas.infrastructure.database
+
---
+++ app/src/main/java/com/marlon/peliculas/infrastructure/database/MovieProvider.kt
@@ -0,0 +1,40 @@
+package com.marlon.peliculas.infrastructure.database
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+class MovieProvider : ContentProvider(){
+
+    override fun getType(uri: Uri): String? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<String>?,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        sortOrder: String?
+    ): Cursor? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
