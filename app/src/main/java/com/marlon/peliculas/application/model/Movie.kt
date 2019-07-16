512a9dedfe3b70c565ced871bb17a82f5d3721cf
Marlon Anaya <desarrolladorsw@unicomfacauca.edu.co>
2019-07-16 16:48:05 -0500

    Json Object

--- app/src/main/java/com/marlon/peliculas/application/model/Movie.kt
+++ app/src/main/java/com/marlon/peliculas/application/model/Movie.kt
@@ -2,12 +2,12 @@ package com.marlon.peliculas.application.model
 
 data class Movie(
     val id: Int = 0,
-    val backdrop_path : String = "",
-    val homepage : String = "",
-    val original_title : String = "",
-    val overview : String = "",
+    val vote_count : String = "",
+    val vote_average : String = "",
+    val title : String = "",
     val popularity : String = "",
     val poster_path : String = "",
-    val vote_average : String = "",
-    val status : String = ""
+    val original_language : String = "",
+    val original_title : String = "",
+    val overview : String = ""
 )
--- app/src/main/java/com/marlon/peliculas/infrastructure/presentation/MainActivity.kt
+++ app/src/main/java/com/marlon/peliculas/infrastructure/presentation/MainActivity.kt
@@ -6,16 +6,29 @@ import android.widget.Toast
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.result.failure
 import com.github.kittinunf.result.success
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import com.google.gson.reflect.TypeToken
 import com.marlon.peliculas.R
+import com.marlon.peliculas.application.coordinator.MovieCoordinator
+import com.marlon.peliculas.application.model.Movie
 import com.marlon.peliculas.infrastructure.core.MovieApplication
 import com.marlon.peliculas.infrastructure.utils.debug
+import javax.inject.Inject
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var movieCoordinator: MovieCoordinator
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (application as MovieApplication).movieComponent.inject(this)
-//        (application as MovieApplication).movieComponent.inject(this)
         setContentView(R.layout.activity_main)
     }
 
@@ -25,13 +38,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setConnection(){
-        Fuel.get("https://api.themoviedb.org/3/movie/550?api_key=e842010883423b76bbe04698fb7eb89e")
+        val movie = hashMapOf("id" to "2")
+        movieCoordinator.addMovie(movie)
+        Fuel.get("https://api.themoviedb.org/3/movie/upcoming?api_key=e842010883423b76bbe04698fb7eb89e")
             .responseString { _, _, result ->
 
                 result.success {
                     val data = result.get()
-
                     debug(data)
+
+                    val gson = Gson()
+                    val map = HashMap<String, Any>()
+                    val list = gson.fromJson(data, map::class.java)
+
+
+
+                    val parser = JsonParser().parse(list["results"].toString())
+
+                    if(parser.isJsonObject){
+                        val movies = parser.asJsonObject
+                        debug("JSON OBJECT ===>>>" + movies)
+                    }
+
+
+
+//                    val gson = GsonBuilder().setPrettyPrinting().create()
+//                    val moviesList: Map<String, Any> = gson.fromJson(data, object : TypeToken<Map<String, Any>>() {}.type)
+//                    moviesList.forEach{
+//                        debug("INGRESO ===> 1 "+it)
+////                        debug("INGRESO ===> 2 "+it.value.toString())
+////                        val movie = hashMapOf(it.key to it.value.toString())
+////                        movieCoordinator.addMovie(hashMapOf(it.key to it.value.toString()))
+////                        movieCoordinator.addMovie(movie)
+////                        debug("INGRESO ===> 2")
+//                    }
+
+
 
                 }
 
